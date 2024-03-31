83cc01d230b50c7836caac853eef5fe3863e6232
Shaik Zuheruddin <119682715+Shaik-Zuher@users.noreply.github.com>
2024-03-31 14:54:34 +0530

    Updated tool.js

--- 1.Major Project(Pdf Merger)/tool.js
+++ 1.Major Project(Pdf Merger)/tool.js
@@ -2,13 +2,12 @@ import PDFMerger from 'pdf-merger-js';
 var merger = new PDFMerger();
 
 export const mergeit=async (p1,p2) => {
-  await merger.add(p1);  //merge all pages. parameter is the path to file and filename.
-  await merger.add(p2); // merge only page 2
+  await merger.add(p1);
+  await merger.add(p2);
   /*await merger.add('pdf2.pdf', [1, 3]); // merge the pages 1 and 3
   await merger.add('pdf2.pdf', '4, 7, 8'); // merge the pages 4, 7 and 8
   await merger.add('pdf3.pdf', '3 to 5'); //merge pages 3 to 5 (3,4,5)
   await merger.add('pdf3.pdf', '3-5'); //merge pages 3 to 5 (3,4,5)
 */
-
   await merger.save('result/merged.pdf');
 };
